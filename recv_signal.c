b88a26642d215d486dcc234621122134ade4bed1
mayorgacruzl <mayorgacruzl@msoe.edu>
2025-11-09 18:00:08 -0600

    Part 4: Sending Data with a Signal

--- recv_signal.c
+++ recv_signal.c
@@ -3,4 +3,43 @@
  * Modified by: Leo Mayorga
  * 
  * Brief summary of program:
- */
+    - registers a SIGUSR1 handle using sigaction with SA_SIGINTO
+    - prints the pid
+*/
+
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+static void handle_usr1(int signo, siginfo_t *info, void *ucontext)
+{
+    (void)signo;
+    (void)ucontext;
+    printf("recv_signal: got SIGUSR1 from pid=%d with value=%d\n", 
+        (int)info->si_pid, info->si_value.sival_int);
+}
+
+int main()
+{
+    // show PID 
+    printf("recv_signal PID: %d\n", getpid());
+
+    struct sigaction sa = {0};
+    sa.sa_sigaction = handle_usr1;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        return 1;
+    }
+
+    // wait forever for signal
+    while(1)
+    {
+        pause(); // sleep until any signal arrives
+    }
+    return 0;
+}
--- send_signal.c
+++ send_signal.c
@@ -3,4 +3,38 @@
  * Modified by: Leo Mayorga
  * 
  * Brief summary of program:
- */
+    - generates a random int to send with SIGUSR1 through sigqueue to a receiver PID
+*/
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) 
+{
+    if (argc != 2) 
+    {
+        fprintf(stderr, "Usage: %s <receiver_pid>\n", argv[0]);
+        return 1;
+    }
+
+    pid_t pid = (pid_t)atoi(argv[1]);
+
+    // seed rng using time
+    srand((unsigned)time(NULL));                    // seed
+    int val = rand();                               // random int
+    printf("send_signal: generated value=%d\n", val);
+
+    union sigval sv;
+    sv.sival_int = val;
+
+    if (sigqueue(pid, SIGUSR1, sv) == -1) 
+    {
+        perror("sigqueue");
+        return 1;
+    }
+    printf("send_signal: sent SIGUSR1 to pid=%d with value=%d\n",
+           (int)pid, val);
+    return 0;
+}
